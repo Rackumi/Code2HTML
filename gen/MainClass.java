0a156b9832325f1ba2f711ceade3ae42ed1fa82e
Rackumi <rackumi4@gmail.com>
2021-04-22 12:15:41 +0200

    Commit v0.14 (Introduccion de gestion de errores)

---
+++ gen/MainClass.java
@@ -0,0 +1,27 @@
+import org.antlr.v4.runtime.*;
+import java.io.IOException;
+
+public class MainClass{
+
+    public static void main(String[] args){
+        try{
+            CharStream input = CharStreams.fromFileName(args[0]);
+            CodeToHTMLLexer analex = new  CodeToHTMLLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(analex);
+
+            CodeToHTMLParser anasint = new CodeToHTMLParser(tokens);
+            anasint.removeErrorListeners();
+            anasint.addErrorListener(new CodeToHTMLErrorListener());
+
+            anasint.r();
+
+        } catch (RecognitionException e) { //Fallo al reconocer la entrada
+            System.err.println("REC " + e.getMessage());
+        } catch (IOException e) { //Fallo de entrada/salida
+            System.err.println("IO " + e.getMessage());
+        } catch (RuntimeException e) { //Cualquier otro fallo
+            System.err.println("RUN " + e.getMessage());
+        }
+    }
+
+}
