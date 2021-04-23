644acfbb8934c611834a8b895d5135b042327148
Rackumi <rackumi4@gmail.com>
2021-04-23 11:24:54 +0200

    Commit v0.19 (Mensajes de error en castellano)

--- gen/CodeToHTMLErrorListener.java
+++ gen/CodeToHTMLErrorListener.java
@@ -7,27 +7,39 @@ import java.util.List;
 
 public class CodeToHTMLErrorListener extends BaseErrorListener{
 
+    int[] contador = new int[1];
+
     public void syntaxError(Recognizer<?, ?> recognizer, Object offendingSymbol, int line, int charPositionInLine, String msg, RecognitionException e){
+
+        contador[0]++;
+        System.err.println("< Error numero "+ contador[0]+" >");
+
         List<String> stack = ((Parser)recognizer).getRuleInvocationStack();
         Collections.reverse(stack);
-        System.err.println("Partes afectadas: "+stack+"\n");
+        System.err.println("Partes afectadas en el error: "+stack+"\n");
+
+        if(msg.contains("mismatched input")){
+            msg = msg.replace("mismatched input","No coincide la entrada");
+            msg = msg.replace("expecting","se esperaba");
+            msg = msg+"\nPuede que este error se deba a que varios caracteres fueron añadidos donde no debian.";
+        }
+        else if(msg.contains("extraneous input")){
+            msg = msg.replace("extraneous input","Entrada no esperada");
+            msg = msg.replace("expecting","se esperaba");
+            msg = msg+"\nPuede que este error se deba a que un caracter fue añadido donde no debia.";
+        }
+        else if(msg.contains("no viable alternative")){
+            msg = msg.replace("no viable alternative at input","No hay una alternativa viable a la entrada obtenida");
+            msg = msg+"\nPuede que este error se deba a que ninguna otra alternativa es consistente con esa regla o subregla.";
+        }
+        else if(msg.contains("missing")){
+            msg = msg.replace("missing","falta");
+            msg = msg.replace("at","en");
+            msg = msg+"\nPuede que este error se deba a que falta un cierto caracter en la ubicacion antes mencionada.";
+        }
 
         System.err.println("linea "+line+":"+charPositionInLine+" en "+offendingSymbol+": "+msg);
         underlineError(recognizer,(Token)offendingSymbol, line, charPositionInLine);
-
-//        StringBuilder buf = new StringBuilder();
-//        buf.append("rule stack: "+stack+" ");
-//        buf.append("line "+line+":"+charPositionInLine+" at "+
-//                offendingSymbol+": "+msg);
-//        JDialog dialog = new JDialog();
-//        Container contentPane = dialog.getContentPane();
-//        contentPane.add(new JLabel(buf.toString()));
-//        contentPane.setBackground(Color.white);
-//        dialog.setTitle("Syntax error");
-//        dialog.pack();
-//        dialog.setLocationRelativeTo(null);
-//        dialog.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-//        dialog.setVisible(true);
     }
 
     protected void underlineError(Recognizer recognizer, Token offendingToken, int line, int charPositionInLine){
@@ -46,6 +58,6 @@ public class CodeToHTMLErrorListener extends BaseErrorListener{
                 System.err.print("^");
             }
         }
-        System.err.println();
+        System.err.println("\n");
     }
 }
